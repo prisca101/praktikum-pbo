5ede9c5a9ce442ce4477129c3a445747a5d16d30
prisca101 <100180208+prisca101@users.noreply.github.com>
2023-02-22 19:47:16 +0700

    Add files via upload

---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,46 @@
+/**
+  * File : Titik.java 22/02/23
+  * Penulis : Prisca Della Budiman
+  * Deskripsi : 
+  * kelas yang berisi program utama kelas Titik
+  *
+  **/
+
+class Titik{
+	double absis;
+	double ordinat;
+	static int counterTitik; //menghitung objek titik
+
+	Titik(double a, double o){
+		counterTitik++;
+		absis = a;
+		ordinat = o;
+	}
+
+	Titik(){
+		counterTitik++;
+		absis = 0;
+		ordinat = 0;
+	}
+
+	void setAbsis(double a){
+		absis = a;
+	}
+
+	void setOrdinat(double o){
+		ordinat = o;
+	}
+
+	double getAbsis(){
+		return absis;
+	}
+
+	double getOrdinat(){
+		return ordinat;
+	}
+
+	int getCounterTitik(){
+		return counterTitik;
+	}
+
+}
