b83fabdcab41a4d5d8f6de50dbefe91ae0b2047b
prisca101 <100180208+prisca101@users.noreply.github.com>
2023-05-17 21:39:21 +0700

    Add files via upload

---
+++ Praktikum 8/BangunDatarGeneric.java
@@ -0,0 +1,29 @@
+/**
+ * File : BangunDatarGeneric.java
+ * Deskripsi : kelas konstruksi generic untuk BangunDatar
+ */
+
+ /**
+  * T diganti dengan Jar
+  Alasan Generic biasa menggunakan T: 
+  untuk kemudahan konvensi dan pembacaan. Huruf "T" sering dikaitkan dengan "Type" 
+  dan mudah dikenali sebagai penanda tempat jenis generik. Hal ini membantu developer 
+  memahami bahwa kode tersebut menggunakan tipe generik dan membuat kode menjadi lebih jelas.
+  */
+
+public class BangunDatarGeneric <Jar extends BangunDatar> {
+    private Jar bangunDatar;
+
+    public void set(Jar tipeBangunDatar){
+        bangunDatar = tipeBangunDatar;
+    }
+
+    public Jar get(){
+        return bangunDatar;
+    }
+
+    public double hitungKeliling(){
+        return bangunDatar.hitungKeliling();
+    }
+
+}
