1181ff5dae2d106a6108bf23ceecb24198ada259
prisca101 <100180208+prisca101@users.noreply.github.com>
2023-05-17 21:39:56 +0700

    Delete Praktikum 8 directory

--- Praktikum 8/BangunDatar.java
+++
@@ -1,7 +0,0 @@
-/**
- * File : BangunDatar.java
- * Deskripsi : kelas abstrak untuk bangun datar
- */
-public abstract class BangunDatar {
-    public abstract double hitungKeliling();
-}
--- Praktikum 8/readme.md
+++
@@ -1,1 +0,0 @@
-Prisca Della Budiman - 24060121140093 - Praktikum PBO C1
