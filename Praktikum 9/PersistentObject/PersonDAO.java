6ddfb37ab3c4df924940290f45c541ac2f9e8f08
prisca101 <100180208+prisca101@users.noreply.github.com>
2023-06-06 16:22:51 +0700

    Add files via upload

---
+++ Praktikum 9/PersistentObject/PersonDAO.java
@@ -0,0 +1,10 @@
+/**
+ * Nama : Prisca Della Budiman
+ * NIM : 24060121140093
+ * File : PersonDAO.java
+ * Deskripsi : interface untuk person access object
+ */
+
+public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+}
